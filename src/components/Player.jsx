bcd9615adf9a72fed6b82bca6d74641ae88fa9e8
Brandon Davis <wowzerss90@yahoo.com>
2024-04-09 17:47:43 -0400

    Used ref to display updated name from input field when clicking button.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,23 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  // lets you reference a value that’s not needed for rendering. Does not re-render Component
+  // DON'T READ OR WRITE DURING RENDERING
+  const inputRef = useRef()
+  
+  const [playerName, setPlayerName] = useState("");
+
+  const handleClick = () => {
+      setPlayerName(inputRef.current.value); 
+      inputRef.current.value = "";
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName || 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={inputRef} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
