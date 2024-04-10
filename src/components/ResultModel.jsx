2e37f17d684956006c9d927b6a508cfe785fa163
Brandon Davis <wowzerss90@yahoo.com>
2024-04-10 12:28:43 -0400

    Changed modal to be render in its own div using a portal

--- src/components/ResultModel.jsx
+++ src/components/ResultModel.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react"
+import { createPortal } from "react-dom";
 
 const ResultModal = forwardRef(function ResultModal({targetTime, timeRemaining, onReset}, ref) {
 
@@ -16,7 +17,7 @@ const ResultModal = forwardRef(function ResultModal({targetTime, timeRemaining, 
         }
     })
 
-    return <dialog ref={dialog} className="result-modal" onClose={onReset}>
+    return createPortal(<dialog ref={dialog} className="result-modal" onClose={onReset}>
         {userLost && <h2>You lost</h2>}
         {!userLost && <h2>Your Score: {score}</h2>}
         <p>The target time was <strong>{targetTime} seconds.</strong></p>
@@ -24,7 +25,8 @@ const ResultModal = forwardRef(function ResultModal({targetTime, timeRemaining, 
         <form method="dialog">
             <button onClick={onReset}>Close</button>
         </form>
-    </dialog>
+    </dialog>, document.getElementById("modal")
+    );
 })
 
 export default ResultModal;
